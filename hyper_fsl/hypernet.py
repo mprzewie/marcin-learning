9acacd63b4cd11d747217fa6ee1695bd037eeebf
Marcin Przewięźlikowski <m.przewie@gmail.com>
2021-07-22 00:47:00 +0200

    hyper-fs(non)l

--- hyper_fsl/hypernet.py
+++ hyper_fsl/hypernet.py
@@ -3,7 +3,7 @@ from typing import Dict
 
 import torch
 from torch import nn, Tensor
-
+from torch.nn import functional as nnf
 
 def get_param_dict(net: nn.Module) -> Dict[str, nn.Parameter]:
     return {
@@ -42,11 +42,15 @@ class HyperNetwork(nn.Module):
         self.n = n
         self.k = k
         self.fe = nn.Sequential(
-            nn.Conv2d(1, 16, 3, padding=1),
+            nn.Conv2d(1+self.n, 64, 3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(16, 32, 3, padding=1, stride=2),
+            nn.Conv2d(64, 64, 3, padding=1),
             nn.ReLU(),
-            nn.Conv2d(32, 64, 3, padding=1, stride=2),
+            nn.Conv2d(64, 64, 3, padding=1, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, padding=1, stride=2),
             nn.Flatten(),
             nn.Linear(7*7*64, fe_out_size),
             nn.ReLU(),
@@ -55,6 +59,12 @@ class HyperNetwork(nn.Module):
         param_head_size = fe_out_size * n * k
 
         param_dict = get_param_dict(target_network)
+
+        param_dict = {
+            name.replace(".", "-"): p
+            for name, p in param_dict.items()
+        }
+
         self.param_shapes = {
             name: p.shape
             for (name, p)
@@ -64,21 +74,21 @@ class HyperNetwork(nn.Module):
         self.param_nets = nn.ModuleDict()
 
         for name, param in param_dict.items():
-            #print(name, param.shape, param_head_size*fe_out_size, fe_out_size*param.numel())
-
-            self.param_nets[name.replace(".", "-")] = nn.Sequential(
+            self.param_nets[name] = nn.Sequential(
                 nn.Linear(param_head_size, fe_out_size),
                 nn.ReLU(),
-                nn.Linear(fe_out_size, param.numel())
+                nn.Linear(fe_out_size, param.numel(), bias=False)
             )
 
         self.target_network = target_network
 
-    def forward(self, support_set: torch.Tensor):
-        assert support_set.shape[0] == self.n * self.k
+    def forward(self, support_set: torch.Tensor, support_labels: torch.Tensor):
+        bs, c, h, w = support_set.shape
+        bl = support_labels.shape[0]
         
-        # embedding of each example in support set
-        emb = self.fe(support_set)
+        assert bs == bl == self.n * self.k, (bs, bl, self.n, self.k)
+        support_set_concat = append_onehot_channels(support_set, support_labels, self.n)
+        emb = self.fe(support_set_concat)
 
         # concat embeddings
         emb = emb.reshape(-1).unsqueeze(0)
@@ -94,7 +104,20 @@ class HyperNetwork(nn.Module):
         return tn
 
 
+def append_onehot_channels(images: torch.Tensor, labels: torch.Tensor, n_classes: int) -> torch.Tensor:
+    """
 
+    Args:
+        images: [b, c, h, w]
+        labels: [b]
 
+    Returns:
+        images with one-hot channels: [b, c+n_classes, h, w]
+    """
 
+    b, c, h, w = images.shape
+    onehots = nnf.one_hot(labels, num_classes=n_classes).float()
+    onehot_channels = onehots.reshape(b, n_classes, 1, 1).repeat(1, 1, h, w).to(images.device) * 100
 
+    images_concat =  torch.cat([onehot_channels, images], dim=1)
+    return images_concat
